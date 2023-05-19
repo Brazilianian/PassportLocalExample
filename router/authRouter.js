4ff310a194bf8ebf29f834e522748b3b48bb748c
Vitalii <vetajika@gmail.com>
2023-05-19 11:15:47 +0300

    refactored auth router

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { AuthController } = require('../controller/authController')
 
-const router = express();
+const router = express.Router();
 
 router.post('/registration', AuthController.registration)
 router.post('/login', AuthController.login)
